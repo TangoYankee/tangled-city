1711b5d1687417f48d130c19c7e61d34616e1ef8
tangoyankee <miller.tim108@gmail.com>
2020-06-23 11:17:13 -0700

    place proxy in docker with hot reload. talk on localhost

--- proxy/src/server.ts
+++ proxy/src/server.ts
@@ -1,12 +1,14 @@
 import * as http from 'http'
 
 const requestHandler = (req: http.IncomingMessage, res: http.ServerResponse) => {
+  res.setHeader('Access-Control-Allow-Origin','*')
+  res.setHeader('Access-Control-Allow-Headers', 'authorization, content-type')
   console.info(req.url)
   res.end()
 }
 
 const proxyServer = http.createServer(requestHandler)
 
-proxyServer.listen(3000, () => {
-  console.info('proxy server running at 3000')
+proxyServer.listen(3000, '0.0.0.0', () => {
+  console.info('proxy running at 3000')
 })
