28cbe787191166bb5a04f251eb17b216569f055f
tangoyankee <miller.tim108@gmail.com>
2020-06-24 09:46:06 -0700

    request from proxy

--- proxy/src/server.ts
+++ proxy/src/server.ts
@@ -3,8 +3,24 @@ import * as http from 'http'
 const requestHandler = (req: http.IncomingMessage, res: http.ServerResponse) => {
   res.setHeader('Access-Control-Allow-Origin','*')
   res.setHeader('Access-Control-Allow-Headers', 'authorization, content-type')
-  console.info(req.url)
-  res.end()
+  console.debug(req.url)
+  let options = {
+    method: 'GET',
+    port: '5984',
+    path: req.url
+  }
+  let proxyReq = http.request(options, res => {
+    let body:any = []
+    res.on('data', chunk => {
+      body.push(chunk)
+    })
+    res.on('end', ()=> {
+      console.log(body)
+    })
+  })
+
+  proxyReq.on('error', (e) => console.warn(e.message))
+  proxyReq.end()
 }
 
 const proxyServer = http.createServer(requestHandler)
